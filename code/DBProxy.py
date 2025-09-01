5d20969e52b58ff012d59ed18d89def70a53d3d2
Dio Ricardo <dio.vieira@live.com>
2025-08-31 22:02:28 -0300

    Minor Fixes and PEP refactor

--- code/DBProxy.py
+++ code/DBProxy.py
@@ -22,6 +22,5 @@ class DBProxy:
     def retrieve_top10(self) -> list:
         return self.connection.execute('SELECT * FROM dados ORDER BY score DESC LIMIT 10').fetchall()
 
-
     def close(self):
         return self.connection.close()
--- code/Score.py
+++ code/Score.py
@@ -23,19 +23,20 @@ class Score:
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.score_text(48, 'YOU WIN!!', C_YELLOW, SCORE_POS['Title'])
+            text = 'Enter Player 1 name (4 characters):'
+            score = player_score[0]
             if game_mode == MENU_OPTION[0]:
                 score = player_score[0]
-                text = 'Enter Player 1 name (4 charcters):'
             if game_mode == MENU_OPTION[1]:
                 score = (player_score[0] + player_score[1]) / 2
-                text = 'Enter Team name (4 charcters):'
+                text = 'Enter Team name (4 characters):'
             if game_mode == MENU_OPTION[2]:
                 if player_score[0] >= player_score[1]:
                     score = player_score[0]
-                    text = 'Player 1 enter your name (4 charcters):'
+                    text = 'Player 1 enter your name (4 characters):'
                 else:
                     score = player_score[1]
-                    text = 'Player 2 enter your name (4 charcters):'
+                    text = 'Player 2 enter your name (4 characters):'
             self.score_text(20, text, C_WHITE, SCORE_POS['EnterName'])
 
             for event in pygame.event.get():
@@ -62,14 +63,14 @@ class Score:
         pygame.mixer_music.play(-1)
         self.window.blit(source=self.surf, dest=self.rect)
         self.score_text(48, 'TOP 10 SCORE', C_YELLOW, SCORE_POS['Title'])
-        self.score_text(20, 'NAME        SCORE          DATE      ', C_YELLOW, SCORE_POS['Label'])
+        self.score_text(20, 'NAME      SCORE          DATE    ', C_YELLOW, SCORE_POS['Label'])
         db_proxy = DBProxy('DBScore')
         list_score = db_proxy.retrieve_top10()
         db_proxy.close()
 
         for player_score in list_score:
             id_, name, score, date = player_score
-            self.score_text(20, f'{name}        {score :05d}          {date}', C_YELLOW,
+            self.score_text(20, f'{name}        {score:05d}          {date}', C_YELLOW,
                             SCORE_POS[list_score.index(player_score)])
 
         while True:
@@ -81,8 +82,6 @@ class Score:
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         return
-
-
 
     def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
